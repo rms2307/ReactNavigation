753238b8aa8f82c41829d04ae92b7631bf78b67e
Robson Silva <robson.silva@tripletech.com.br>
2021-01-24 18:33:07 -0300

    Update TelaC.js

--- src/views/TelaC.js
+++ src/views/TelaC.js
@@ -3,7 +3,7 @@ import TextoCentral from '../components/TextoCentral'
 
 export default props => {
     return (
-        <TextoCentral corFundo='#9932cd'>
+        <TextoCentral corFundo='#9932ce'>
             Tela C = {props.route.params.numero}
         </TextoCentral>
     )
